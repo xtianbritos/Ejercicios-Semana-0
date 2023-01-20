503fc90bc315dd1e39ed3fd2d4a56eeb21990ea2
Xtian <xtiancuenta1@gmail.com>
2023-01-20 09:56:49 -0300

    Se mejoró la función del ejercicio 2 y se agregó pruebas

--- ejercicio2.ts
+++ ejercicio2.ts
@@ -1,3 +1,7 @@
-const potencia = (base: number, exponente: number): number => {
-    return Math.pow(base, exponente);
+const potencia = (base: number, exponente: number): void => {
+    console.log(`El número ${base} elevado al exponente ${exponente} da ${Math.pow(base, exponente)}`);
 }
+
+potencia(5, 3);
+potencia(20, 2);
+potencia(1, 20);
